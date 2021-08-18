440baac1a74c69190a7f9028ecd29f32ea33d9ea
unknown <jiadongwangbuaa@163.com>
2021-08-18 13:23:25 +0800

    l

--- hh.sh
+++ hh.sh
@@ -1,2 +1,7 @@
 kubectl apply -k "manifests/kustomize/cluster-scoped-resources"
 kubectl apply -k "manifests/kustomize/env/platform-agnostic-pns"
+sodu service docker start
+minikube start
+kubectl get pods -n kubeflow
+kubectl port-forward -n kubeflow svc/ml-pipeline-ui 8082:80
+dsl-compile --py ${DIR}/lll.py --output ${DIR}/lll.tar.gz
